d2243ba8625a3a383501c021701908ca3ec8fd72
saketbhatnagar1 <165547660+saketbhatnagar1@users.noreply.github.com>
2024-05-30 23:59:07 +0530

    Add files via upload

---
+++ 01pracitcejs/expressjs.js
@@ -0,0 +1,37 @@
+const http = require("http")
+const fs = require("fs")
+const url = require("url")
+const express = require("express")
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    return res.send("HOME PAGE BI*H")
+})
+
+app.get('/about',(req,res)=>{
+    return res.send("U WNAT TO KNOW ABOUT US "+`${req.query.name}` +" BRO ?")
+})
+
+// app.post('/about',(req,res)=>{
+//     return res.send("U ARE "+req.query.name+"OLD")
+// })
+
+
+app.listen(7000,()=>console.log("SERVER STARTED"))//=> we can use this method instead of the http methods below
+
+// const server2 = http.createServer(app)
+// server2.listen(7000,()=>console.log("SERVER STARTED"))
+
+//versioning:
+/*
+version = 4.18.2
+
+1stpart=4
+2ndpart=18 ->
+3rdpart=2 ->minor fixes(Optional)
+
+
+
+
+*/
