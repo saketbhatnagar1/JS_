c442273102f9de85ecc961b6d3e4b812bd949000
saketbhatnagar1 <165547660+saketbhatnagar1@users.noreply.github.com>
2024-11-13 14:31:28 +0530

    Update arraymethods.js

--- 01pracitcejs/arraymethods.js
+++ 01pracitcejs/arraymethods.js
@@ -21,7 +21,7 @@ console.log(arr); // Output: [0, 1, 2, 3]
 
 let arr4 = [1, 2];
 let arr5 = [3, 4];
-let mergedArray = arr1.concat(arr2);
+let mergedArray = arr4.concat(arr5);
 console.log(mergedArray); // Output: [1, 2, 3, 4]
 
 
